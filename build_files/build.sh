4abcff8ac8fecbeae76c7e01c9f25467d50be489
Tyler <tyler@caffeinatedlinux.com>
2026-01-02 09:16:31 -0600

    addviv

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,9 +12,10 @@ set -ouex pipefail
 # this installs a package from fedora repos
 
 dnf5 config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+dnf5 config-manager addrepo --from-repofile=https://repo.vivaldi.com/stable/vivaldi-fedora.repo
 dnf5 group install -y cosmic-desktop
 dnf5 group install -y cosmic-desktop-apps
-dnf5 install -y tmux zsh fastfetch lm_sensors oddjob make oddjob-mkhomedir freeipa-client tailscale
+dnf5 install -y tmux zsh fastfetch lm_sensors oddjob make oddjob-mkhomedir freeipa-client tailscale vivaldi-stable
 
 # Use a COPR Example:
 #
@@ -26,4 +27,6 @@ dnf5 install -y tmux zsh fastfetch lm_sensors oddjob make oddjob-mkhomedir freei
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+
+
 
