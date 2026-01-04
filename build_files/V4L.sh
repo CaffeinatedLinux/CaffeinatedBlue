87eedc0e4c3e46c6ec99be8ce5c665ee608f724f
Tyler Brown <tyler@sharpedo.caffeinatedlinux.com>
2026-01-04 15:23:20 -0600

    V4L KMod

--- build_files/V4L.sh
+++ build_files/V4L.sh
@@ -6,8 +6,9 @@ set -ouex pipefail
 dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Install Packages
+ls /opt/akmods-common
 
-dnf5 install -y v4l2loopback /tmp/akmods-common/rpms/kmods/kmod-v4l2loopback*.rpm
+dnf5 install -y v4l2loopback /opt/akmods-common/rpms/kmods/kmod-v4l2loopback*.rpm
 
 # Remove RPMFusion
 dnf5 -y remove rpmfusion-free-release
